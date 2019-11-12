a03a9a1f7be87f86d47e4dc9a32159980973e179
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2019-11-12 00:17:55 -0300

    feat(): adjusts migrations, handles tenant creation for orgs

---
+++ mockserver/tenants/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from tenants.models import Organization
+
+
+admin.site.register(Organization)
--- mockserver/tenants/apps.py
+++ mockserver/tenants/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class TenantsConfig(AppConfig):
     name = 'tenants'
+
+    def ready(self):
+        import tenants.signals  # noqa
