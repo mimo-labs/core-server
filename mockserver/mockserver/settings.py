0b3777a3d31fae68f417ef3540d6a1bccac170ac
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2019-11-27 21:38:03 -0300

    chore(): undo previous settings adjustments regarding redoc

--- mockserver/mockserver/settings.py
+++ mockserver/mockserver/settings.py
@@ -74,7 +74,7 @@ REST_FRAMEWORK = {
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'mockserver', 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
