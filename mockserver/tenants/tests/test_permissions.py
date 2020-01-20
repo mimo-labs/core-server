0ea119ecf4b6449229efd73de57a10fe348c1b9d
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2020-01-19 21:23:01 -0300

    feat(): exposes organization crud

--- mockserver/tenants/tests/test_permissions.py
+++ mockserver/tenants/tests/test_permissions.py
@@ -1,5 +1,3 @@
-from unittest.mock import Mock
-
 from django.contrib.auth.models import AnonymousUser
 from django.test import TestCase
 from rest_framework.test import APIRequestFactory
@@ -15,35 +13,6 @@ class TenantPermissionTestCase(MockTestMixin, TestCase):
         cls.tenant = cls.create_bare_minimum_tenant()
         cls.user = cls.tenant.user_ptr
         cls.factory = APIRequestFactory()
-
-    def test_create_request_is_allowed(self):
-        view = Mock()
-        view.action = 'create'
-
-        is_allowed = self.permission.has_permission(None, view)
-
-        self.assertTrue(is_allowed)
-
-    def test_unauthenticated_user_is_denied(self):
-        request = self.factory.post('/some/url')
-        request.user = AnonymousUser()
-        view = Mock()
-        view.action = 'list'
-
-        is_allowed = self.permission.has_permission(request, view)
-
-        self.assertFalse(is_allowed)
-
-    def test_authenticated_list_is_allowed(self):
-        request = self.factory.get('/some/url')
-        request.user = Mock()
-        request.user.is_anonymous = False
-        view = Mock()
-        view.action = 'list'
-
-        is_allowed = self.permission.has_permission(request, view)
-
-        self.assertTrue(is_allowed)
 
     def test_unauthenticated_detail_permission_is_denied(self):
         request = self.factory.delete('/some/url')
