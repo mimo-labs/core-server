0bf052bd4231223466dadc545393f5426e8d1923
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2020-01-19 22:01:36 -0300

    chore(): adds organization permission and serializer tests

--- mockserver/tenants/tests/test_permissions.py
+++ mockserver/tenants/tests/test_permissions.py
@@ -1,9 +1,11 @@
-from django.contrib.auth.models import AnonymousUser
 from django.test import TestCase
 from rest_framework.test import APIRequestFactory
 
 from common.tests.mixins import MockTestMixin
-from tenants.permissions import TenantPermission
+from tenants.permissions import (
+    TenantPermission,
+    OrganizationPermission
+)
 
 
 class TenantPermissionTestCase(MockTestMixin, TestCase):
@@ -14,18 +16,28 @@ class TenantPermissionTestCase(MockTestMixin, TestCase):
         cls.user = cls.tenant.user_ptr
         cls.factory = APIRequestFactory()
 
-    def test_unauthenticated_detail_permission_is_denied(self):
-        request = self.factory.delete('/some/url')
-        request.user = AnonymousUser()
-
-        is_allowed = self.permission.has_object_permission(request, None, self.tenant)
-
-        self.assertFalse(is_allowed)
-
     def test_tenant_owner_user_is_allowed(self):
         request = self.factory.delete('/some/url')
         request.user = self.user
 
         is_allowed = self.permission.has_object_permission(request, None, self.tenant)
+
+        self.assertTrue(is_allowed)
+
+
+class OrganizationPermissionTestCase(MockTestMixin, TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.permission = OrganizationPermission()
+        cls.tenant = cls.create_bare_minimum_tenant()
+        cls.organization = cls.create_bare_minimum_organization(cls.tenant)
+        cls.factory = APIRequestFactory()
+
+    # TODO: Once models are improved tighten up permission checking
+    def test_tenant_in_organization_is_allowed(self):
+        request = self.factory.delete('/some/url')
+        request.user = self.tenant.user_ptr
+
+        is_allowed = self.permission.has_object_permission(request, None, self.organization)
 
         self.assertTrue(is_allowed)
--- mockserver/tenants/tests/test_serializers.py
+++ mockserver/tenants/tests/test_serializers.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory
 
 from common.tests.mixins import MockTestMixin
-from tenants.serializers import TenantSerializer
+from tenants.serializers import (
+    TenantSerializer,
+    OrganizationSerializer
+)
 
 
 class TenantSerializerValidationTestCase(MockTestMixin, TestCase):
@@ -20,3 +24,24 @@ class TenantSerializerValidationTestCase(MockTestMixin, TestCase):
         new_passwd = serializer.validate_password(data['password'])
 
         self.assertNotEqual(old_password, new_passwd)
+
+
+class OrganizationSerializerTestCase(MockTestMixin, TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.serializer = OrganizationSerializer
+        cls.tenant = cls.create_bare_minimum_tenant()
+        cls.factory = APIRequestFactory()
+
+    def test_created_organization_automatically_adds_authenticated_user(self):
+        data = {
+            'name': 'aaaaaaaaa'
+        }
+        request = self.factory.post('/some/url')
+        request.user = self.tenant.user_ptr
+        serializer = self.serializer(data=data, context={'request': request})
+        serializer.is_valid()
+
+        organization = serializer.save()
+
+        self.assertIn(self.tenant, organization.users.all())
