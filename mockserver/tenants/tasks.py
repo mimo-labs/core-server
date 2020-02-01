154cd9f7a93d26092e23b11c0e332a0d8672561b
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2020-02-01 13:51:14 -0300

    fix(): removes circular import from tenants tasks

--- mockserver/tenants/tasks.py
+++ mockserver/tenants/tasks.py
@@ -3,14 +3,13 @@ import logging
 from celery.task import task
 
 from tenants.invites.strategy import UserMailingStrategy
-from tenants.models import OrganizationInvite
 
 
 logger = logging.getLogger(__name__)
 
 
 @task()
-def mail_membership_invite(invite: OrganizationInvite):
+def mail_membership_invite(invite):
     is_existing = invite.tenant is not None
     logger.info(f"sending org f{invite.organization.name} invite is_existing {is_existing}")
     mailing = UserMailingStrategy.get_strategy(is_existing)
