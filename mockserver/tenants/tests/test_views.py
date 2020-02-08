cf926f8feee5ca86618717282bda5ff43358b482
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2020-02-08 13:37:27 -0300

    chore(): corrects organization view tests, adds orginvite model tests

--- mockserver/tenants/tests/test_views.py
+++ mockserver/tenants/tests/test_views.py
@@ -19,30 +19,43 @@ class OrganizationViewSetTestCase(MockTestMixin, APITestCase):
             url = reverse('v1:organization-list')
             response = self.client.get(url)
 
-            self.assertEquals(response.status_code, 401)
+            self.assertEqual(response.status_code, 401)
 
         with self.subTest('detail should be disallowed'):
-            url = reverse('v1:organization-detail', kwargs={'pk': 1})
+            url = reverse('v1:organization-detail', kwargs={'pk': self.organization.pk})
             response = self.client.get(url)
 
-            self.assertEquals(response.status_code, 401)
+            self.assertEqual(response.status_code, 401)
+
+        with self.subTest('member invite should be disallowed'):
+            url = reverse('v1:organization-member-invite', kwargs={'pk': self.organization.pk})
+            response = self.client.post(url)
+
+            self.assertEqual(response.status_code, 401)
 
     def test_authenticated_non_organization_member_requests_are_disallowed(self):
-        url = reverse('v1:organization-detail', kwargs={'pk': 1})
+        url = reverse('v1:organization-detail', kwargs={'pk': self.organization.pk})
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.tenant.user_ptr.auth_token}')
         with self.subTest('retrieve should be disallowed'):
             response = self.client.get(url)
 
-            self.assertEquals(response.status_code, 401)
+            self.assertEqual(response.status_code, 403)
 
         with self.subTest('update should be disallowed'):
             response = self.client.put(url)
 
-            self.assertEquals(response.status_code, 401)
+            self.assertEqual(response.status_code, 403)
 
         with self.subTest('delete should be disallowed'):
             response = self.client.delete(url)
 
-            self.assertEquals(response.status_code, 401)
+            self.assertEqual(response.status_code, 403)
+
+        with self.subTest('member invite should be disallowed'):
+            url = reverse('v1:organization-member-invite', kwargs={'pk': self.organization.pk})
+            response = self.client.post(url, **{'HTTP_HOST': 'foobar'})
+
+            self.assertEqual(response.status_code, 403)
 
     @patch('tenants.views.Organization.objects', new=MagicMock())
     def test_authenticated_list_requests_are_allowed(self):
@@ -51,16 +64,16 @@ class OrganizationViewSetTestCase(MockTestMixin, APITestCase):
         with self.subTest('list should be allowed'):
             response = self.client.get(url)
 
-            self.assertEquals(response.status_code, 200)
+            self.assertEqual(response.status_code, 200)
 
         with self.subTest('create should be allowed'):
             response = self.client.post(url)
 
-            self.assertEquals(response.status_code, 400)  # invalid but allowed creation request
+            self.assertEqual(response.status_code, 400)  # invalid but allowed creation request
 
     @patch('tenants.views.Organization.objects', new=MagicMock())
     def test_authenticated_member_detail_is_allowed(self):
-        url = reverse('v1:organization-detail', kwargs={'pk': self.organization.id})
+        url = reverse('v1:organization-detail', kwargs={'pk': self.organization.pk})
         self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.tenant.user_ptr.auth_token}')
         self.organization.organizationmembership_set.create(
             organization=self.organization,
@@ -72,7 +85,7 @@ class OrganizationViewSetTestCase(MockTestMixin, APITestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_authenticated_admin_update_is_allowed(self):
-        url = reverse('v1:organization-detail', kwargs={'pk': self.organization.id})
+        url = reverse('v1:organization-detail', kwargs={'pk': self.organization.pk})
         self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.tenant.user_ptr.auth_token}')
         self.organization.organizationmembership_set.create(
             organization=self.organization,
@@ -85,7 +98,7 @@ class OrganizationViewSetTestCase(MockTestMixin, APITestCase):
         self.assertEqual(response.status_code, 400)  # invalid but allowed update request
 
     def test_authenticated_owner_delete_is_allowed(self):
-        url = reverse('v1:organization-detail', kwargs={'pk': self.organization.id})
+        url = reverse('v1:organization-detail', kwargs={'pk': self.organization.pk})
         self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.tenant.user_ptr.auth_token}')
         self.organization.organizationmembership_set.create(
             organization=self.organization,
@@ -96,3 +109,42 @@ class OrganizationViewSetTestCase(MockTestMixin, APITestCase):
         response = self.client.delete(url)
 
         self.assertEqual(response.status_code, 204)
+
+    @patch('tenants.views.OrganizationViewSet.get_serializer')
+    def test_empty_body_calls_no_invites(self, patch_serializer):
+        body = {
+            'emails': []
+        }
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.tenant.user_ptr.auth_token}')
+        self.organization.organizationmembership_set.create(
+            organization=self.organization,
+            tenant=self.tenant,
+            is_owner=True
+        )
+        url = reverse('v1:organization-member-invite', kwargs={'pk': self.organization.pk})
+
+        response = self.client.post(url, body, format='json', **{'HTTP_HOST': 'foobar'})
+
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(response.content, b'')
+        self.assertFalse(patch_serializer.called)
+
+    @patch('tenants.views.OrganizationViewSet.get_serializer')
+    def test_n_emails_call_n_invites(self, patch_serializer):
+        emails = ['foo@bar.baz', 'bar@baz.quux', 'yet@another.mail']
+        body = {
+            'emails': emails
+        }
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.tenant.user_ptr.auth_token}')
+        self.organization.organizationmembership_set.create(
+            organization=self.organization,
+            tenant=self.tenant,
+            is_owner=True
+        )
+        url = reverse('v1:organization-member-invite', kwargs={'pk': self.organization.pk})
+
+        response = self.client.post(url, body, format='json', **{'HTTP_HOST': 'foobar'})
+
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(response.content, b'')
+        self.assertEqual(patch_serializer.call_count, len(emails))
