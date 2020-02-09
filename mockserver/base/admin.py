98902d132a25e7942d72d577829719c34427bb3f
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2020-02-09 20:42:06 -0300

    chore(): corrects linting issues

--- mockserver/base/admin.py
+++ mockserver/base/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from base.models import Technology
+
+
+@admin.register(Technology)
+class TechnologyAdmin(admin.ModelAdmin):
+    pass
--- mockserver/base/models.py
+++ mockserver/base/models.py
@@ -6,3 +6,9 @@ from common.models import DateAwareModel
 class Technology(DateAwareModel):
     code = models.CharField(max_length=3)
     name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = "Technologies"
+
+    def __str__(self):
+        return f"{self.name} ({self.code})"
