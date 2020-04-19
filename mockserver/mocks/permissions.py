6ca0353d7cc0131e7cba339b229b640dd2b0644b
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2020-04-19 01:37:09 -0300

    fix(): corrects handling of org based permission for resources

--- mockserver/mocks/permissions.py
+++ mockserver/mocks/permissions.py
@@ -1,17 +1,16 @@
 from rest_framework import permissions
 
-from tenants.models import Organization
-
 
 class IsOwnOrganization(permissions.BasePermission):
     def has_permission(self, request, view):
+        return True
+
+    def has_object_permission(self, request, view, obj):
         if request.user.is_anonymous:  # Requires login
             return False
 
-        organization_id = request.data['organization']
-
-        try:
-            organization = Organization.objects.get(id=organization_id)
-            return request.user.tenant in organization.users.all()
-        except Organization.DoesNotExist:
-            return False
+        return obj.organization_id in [
+            org.id for
+            org in
+            request.user.tenant.organizations.all()
+        ]
