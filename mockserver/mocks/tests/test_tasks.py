cde5d321fcd0008ad25e216e4668b24346621c36
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2020-08-17 20:54:35 -0300

    chore(): adds tests for new feature

---
+++ mockserver/mocks/tests/test_tasks.py
@@ -0,0 +1,39 @@
+from unittest.mock import patch
+
+from django.test import TransactionTestCase
+
+from common.tests.mixins import MockTestMixin
+from mocks.tasks import delete_empty_endpoint
+
+
+class EndpointCleanupTaskTestCase(MockTestMixin, TransactionTestCase):
+    task = delete_empty_endpoint
+
+    def setUp(self):
+        super().setUp()
+        self.patcher = patch('mocks.tasks.EndpointService.cleanup_endpoint')
+        self.mock_service = self.patcher.start()
+        self.addCleanup(self.patcher.stop)
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.project = cls.create_bare_minimum_project()
+        cls.endpoint = cls.create_bare_minimum_endpoint(project=cls.project)
+        cls.mock = cls.create_bare_minimum_mock(project=cls.project, endpoint=cls.endpoint)
+
+    def test_run_with_null_mock_does_nothing(self):
+        self.task(None)
+
+        self.assertFalse(self.mock_service.called)
+
+    def test_run_with_unique_mock_calls_service_method(self):
+        self.task(self.mock)
+
+        self.assertTrue(self.mock_service)
+
+    def test_run_with_multiple_mocks_calls_service_method(self):
+        other_mock = self.create_bare_minimum_mock(project=self.project, endpoint=self.endpoint)
+        self.task(other_mock)
+
+        self.assertTrue(self.mock_service)
