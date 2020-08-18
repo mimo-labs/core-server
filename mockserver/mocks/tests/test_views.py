c9476f8b9fcc075482153d764e2ab65b4ae37522
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2020-08-17 21:21:26 -0300

    chore(): removes deprecated test cases

--- mockserver/mocks/tests/test_views.py
+++ mockserver/mocks/tests/test_views.py
@@ -135,56 +135,6 @@ class CategoryViewSetTestCase(APIViewSetTestCase):
         self.assertEqual(3, len(response.json()))
 
 
-class EndpointViewSetTestCase(APIViewSetTestCase):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.token = Token.objects.get(user=cls.tenant)
-
-    def test_list_without_project_id_fails(self):
-        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.token}')
-        url = reverse('v1:endpoint-list')
-
-        response = self.client.get(url)
-
-        self.assertEqual(400, response.status_code)
-        self.assertEqual('project_id is required', response.json()['detail'])
-
-    def test_list_with_unauthorized_project_fails(self):
-        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.token}')
-        url = reverse('v1:endpoint-list')
-        other_organization = self.create_bare_minimum_organization()
-        other_project = self.create_bare_minimum_project(other_organization)
-
-        response = self.client.get(url, {'project_id': other_project.id})
-
-        self.assertEqual(403, response.status_code)
-
-    def test_list_with_nonexistent_project_returns_404(self):
-        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.token}')
-        url = reverse('v1:endpoint-list')
-        last_project_id = Project.objects.last().id
-
-        response = self.client.get(url, {'project_id': last_project_id + 1})
-
-        self.assertEqual(404, response.status_code)
-
-    def test_with_user_project_returns_filtered(self):
-        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.token}')
-        url = reverse('v1:endpoint-list')
-        other_organization = self.create_bare_minimum_organization()
-        other_project = self.create_bare_minimum_project(other_organization)
-        # create an endpoint on a different project
-        self.create_bare_minimum_endpoint(self.project)
-        self.create_bare_minimum_endpoint(other_project)
-
-        response = self.client.get(url, {'project_id': self.project.id})
-
-        self.assertEqual(200, response.status_code)
-        # 2 endpoints: one created now, one for the base case mock
-        self.assertEqual(2, len(response.json()))
-
-
 class HttpVerbViewSetTestCase(APIViewSetTestCase):
     @classmethod
     def setUpClass(cls):
