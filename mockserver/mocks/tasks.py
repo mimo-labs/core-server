0a2ce7de3bab0d8d4e4f95740d014bda7847f8cb
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2020-08-30 16:32:54 -0300

    chore(): adjusts celery to pass secure data

--- mockserver/mocks/tasks.py
+++ mockserver/mocks/tasks.py
@@ -4,24 +4,20 @@ import time
 from celery.task import task
 from django.conf import settings
 
-from mocks.models import Mock
 from mocks.services import EndpointService
 
 
 logger = logging.getLogger(__name__)
 
 
-@task(serializer="pickle")
-def delete_empty_endpoint(mock: Mock):
-    # Defensive guard
-    if not mock:
-        return
-
-    logger.info("begin clear check endpoint %s" % mock.path)
+@task()
+def delete_empty_endpoint(path_name: str, project_id: int):
+    logger.info("begin clear check endpoint %s" % path_name)
     if settings.DEBUG:
         start = time.time()
 
-    EndpointService.cleanup_endpoint(mock.path)
+    path = EndpointService.get_endpoint_by_name_and_project(path_name, project_id)
+    EndpointService.cleanup_endpoint(path)
 
     logger.info("finished clear check endpoint")
 
